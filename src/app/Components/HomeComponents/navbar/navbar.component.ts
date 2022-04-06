1dee7e1a94bc5ad25a9312510431df54404e3607
UsielOM <usiel31197@outlook.es>
2022-04-06 00:41:45 -0500

    Pdfmaker img

--- src/app/Components/HomeComponents/navbar/navbar.component.ts
+++ src/app/Components/HomeComponents/navbar/navbar.component.ts
@@ -1,21 +1,84 @@
 import { Component, OnInit } from '@angular/core';
-import { PdfMakeWrapper, Txt } from 'pdfmake-wrapper';
+import { Canvas, Ellipse, Img, PdfMakeWrapper, Txt } from 'pdfmake-wrapper';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-  generatePDF(){
-const pdf = new PdfMakeWrapper();
-pdf.add(
-  new Txt('Hola Mundo').bold().end
-);
-pdf.create().open();
-  }
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+title = 'angular-pdfmakewrapper';
+
+generatePDF(){
+  
+
+  const pdf = new PdfMakeWrapper();
+  pdf.styles({
+    style1: {
+
+        height: 50,
+        width: 50
+
+    },
+    style2: {
+        italics: true
+    },
+    Img:{
+      height: 20,
+      width: 20
+
+    },
+    
+    
+});
+
+
+  pdf.header('This is a header');
+
+
+
+  pdf.add(
+   new Txt("Hello World").bold().italics().end
+  );
+
+
+  pdf.footer('This is a footer');
+
+const generate = async () =>{
+ 
+//   pdf.add(
+//     new Canvas([
+//         new Ellipse([10, 10], [30, 30]).end
+        
+
+//     ]
+//     ).end
+// );
+
+pdf.add( await new  Img('assets/F.png').build());
+  pdf.create().open();
+  
+}
+
+
+
+generate();
 
 }
+
+  constructor() { }
+
+  
+
+  ngOnInit(): void {
+
+
+}
+
+
+
+}
+
+
+
